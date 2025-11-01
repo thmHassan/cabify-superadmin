366955fd72cb625f533e4a5e55f297fbf0953457
Denish Prajapati <d100091996@gmail.com>
2025-11-01 18:45:15 +0530

    changes

--- src/components/configs/app.config.js
+++ src/components/configs/app.config.js
@@ -1,11 +1,11 @@
 import { SELECTION_PATH } from "../../constants/routes.path.constant/auth.route.path.constant";
 
 const appConfig = {
-  apiPrefix: import.meta.env.VITE_API_BASE_URL || "https://backend.cabifyit.com/api",
+  apiPrefix: "https://backend.cabifyit.com/api",
   authenticatedEntryPath: "/overview",
   unAuthenticatedEntryPath: SELECTION_PATH,
   locale: "en",
-  enableMock: false,
+  enableMock: true,
 };
 
 export default appConfig;
